67e05db81c38a1cc3eb9220047dcda9aa5b702ac
Jayant962 <jayantdhingra370@gmail.com>
2026-03-31 23:11:16 +0530

    connect login and signup using flask

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/signup")
 def home():
     return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
