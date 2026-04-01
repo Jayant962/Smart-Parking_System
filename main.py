4ea41bd35e4e343d8d9a58ef125e151d70f2ee40
Jayant962 <jayantdhingra370@gmail.com>
2026-04-01 10:03:58 +0530

    Changed HTML structure for Flask and started working on booking page

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/signup")
+@app.route("/")
+def start():
+    return render_template("login.html")
+
+@app.route("/signup", methods=["GET", "POST"])
 def home():
-    return render_template('signup.html')
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        return render_template("booking.html")   # or redirect later
+
+    return render_template("signup.html")
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route("/submit",methods=["GET","POST"])
+def submit():
+    return render_template('booking.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
